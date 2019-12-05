2a8a5cb83b4cdfffa5c6b7a9d04f9db33415b09c
dhifandiandra <dhifand@gmail.com>
2019-12-05 21:00:08 +0700

    All done

--- app/src/main/java/com/example/projectsispak/ActivityAuth.java
+++ app/src/main/java/com/example/projectsispak/ActivityAuth.java
@@ -14,7 +14,7 @@ public class ActivityAuth extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.AuthFragment);
+        setContentView(R.layout.authfragment);
         setFragment(authFragment);
     }
 
--- app/src/main/java/com/example/projectsispak/JenisPenyakit/detailPenyakitKucing.java
+++ app/src/main/java/com/example/projectsispak/JenisPenyakit/detailPenyakitKucing.java
@@ -47,99 +47,99 @@ public class detailPenyakitKucing extends AppCompatActivity {
 
     private void setDeskripsiPenyakit(String deskripsiPenyakit){
         TextView deskripsiPenyakit1 = findViewById(R.id.deskripsiPenyakit);
-//        deskripsiPenyakit1.setText(deskripsiPenyakit);
-        if (namaPenyakit1.getText().equals("Scabies")){
-            deskripsiPenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
-        } else if (namaPenyakit1.getText().equals("Gastritis")){
-            deskripsiPenyakit1.setText("Ini deskripsi gastritis");
-        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
-            deskripsiPenyakit1.setText("helmiiiiiiinnnnnnnnnn");
-        } else if (namaPenyakit1.getText().equals("Rhintis")){
-            deskripsiPenyakit1.setText("rhintiiiiiiiiissssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermatitis")){
-            deskripsiPenyakit1.setText("dermatitiiiiisssssssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
-            deskripsiPenyakit1.setText("dermaphytttttttttttttttttttt");
-        } else if (namaPenyakit1.getText().equals("Otitis")){
-            deskripsiPenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
-        } else if (namaPenyakit1.getText().equals("Entritis")){
-            deskripsiPenyakit1.setText("entririiiiiiiiiiiiii");
-        } else if (namaPenyakit1.getText().equals("Sehat")){
-            deskripsiPenyakit1.setText("sehat coooooooyyyyyyyyy");
-        }
+        deskripsiPenyakit1.setText(deskripsiPenyakit);
+//        if (namaPenyakit1.getText().equals("Scabies")){
+//            deskripsiPenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
+//        } else if (namaPenyakit1.getText().equals("Gastritis")){
+//            deskripsiPenyakit1.setText("Ini deskripsi gastritis");
+//        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
+//            deskripsiPenyakit1.setText("helmiiiiiiinnnnnnnnnn");
+//        } else if (namaPenyakit1.getText().equals("Rhintis")){
+//            deskripsiPenyakit1.setText("rhintiiiiiiiiissssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermatitis")){
+//            deskripsiPenyakit1.setText("dermatitiiiiisssssssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
+//            deskripsiPenyakit1.setText("dermaphytttttttttttttttttttt");
+//        } else if (namaPenyakit1.getText().equals("Otitis")){
+//            deskripsiPenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
+//        } else if (namaPenyakit1.getText().equals("Entritis")){
+//            deskripsiPenyakit1.setText("entririiiiiiiiiiiiii");
+//        } else if (namaPenyakit1.getText().equals("Sehat")){
+//            deskripsiPenyakit1.setText("sehat coooooooyyyyyyyyy");
+//        }
     }
 
     private void setGejalaPenyakit(String gejalaPenyakit){
         TextView gejalapenyakit1 = findViewById(R.id.gejalaPenyakit);
-//        gejalapenyakit1.setText(gejalaPenyakit);
-        if (namaPenyakit1.getText().equals("Scabies")){
-            gejalapenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
-        } else if (namaPenyakit1.getText().equals("Gastritis")){
-            gejalapenyakit1.setText("Ini deskripsi gastritis");
-        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
-            gejalapenyakit1.setText("helmiiiiiiinnnnnnnnnn");
-        } else if (namaPenyakit1.getText().equals("Rhintis")){
-            gejalapenyakit1.setText("rhintiiiiiiiiissssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermatitis")){
-            gejalapenyakit1.setText("dermatitiiiiisssssssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
-            gejalapenyakit1.setText("dermaphytttttttttttttttttttt");
-        } else if (namaPenyakit1.getText().equals("Otitis")){
-            gejalapenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
-        } else if (namaPenyakit1.getText().equals("Entritis")){
-            gejalapenyakit1.setText("entririiiiiiiiiiiiii");
-        } else if (namaPenyakit1.getText().equals("Sehat")){
-            gejalapenyakit1.setText("sehat coooooooyyyyyyyyy");
-        }
+        gejalapenyakit1.setText(gejalaPenyakit);
+//        if (namaPenyakit1.getText().equals("Scabies")){
+//            gejalapenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
+//        } else if (namaPenyakit1.getText().equals("Gastritis")){
+//            gejalapenyakit1.setText("Ini deskripsi gastritis");
+//        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
+//            gejalapenyakit1.setText("helmiiiiiiinnnnnnnnnn");
+//        } else if (namaPenyakit1.getText().equals("Rhintis")){
+//            gejalapenyakit1.setText("rhintiiiiiiiiissssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermatitis")){
+//            gejalapenyakit1.setText("dermatitiiiiisssssssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
+//            gejalapenyakit1.setText("dermaphytttttttttttttttttttt");
+//        } else if (namaPenyakit1.getText().equals("Otitis")){
+//            gejalapenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
+//        } else if (namaPenyakit1.getText().equals("Entritis")){
+//            gejalapenyakit1.setText("entririiiiiiiiiiiiii");
+//        } else if (namaPenyakit1.getText().equals("Sehat")){
+//            gejalapenyakit1.setText("sehat coooooooyyyyyyyyy");
+//        }
     }
 
     private void setPenyebabPenyakit(String penyebabPenyakit){
         TextView penyebabpenyakit1 = findViewById(R.id.penyebabPenyakit);
-//        penyebabpenyakit1.setText(penyebabPenyakit);
-        if (namaPenyakit1.getText().equals("Scabies")){
-            penyebabpenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
-        } else if (namaPenyakit1.getText().equals("Gastritis")){
-            penyebabpenyakit1.setText("Ini deskripsi gastritis");
-        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
-            penyebabpenyakit1.setText("helmiiiiiiinnnnnnnnnn");
-        } else if (namaPenyakit1.getText().equals("Rhintis")){
-            penyebabpenyakit1.setText("rhintiiiiiiiiissssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermatitis")){
-            penyebabpenyakit1.setText("dermatitiiiiisssssssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
-            penyebabpenyakit1.setText("dermaphytttttttttttttttttttt");
-        } else if (namaPenyakit1.getText().equals("Otitis")){
-            penyebabpenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
-        } else if (namaPenyakit1.getText().equals("Entritis")){
-            penyebabpenyakit1.setText("entririiiiiiiiiiiiii");
-        } else if (namaPenyakit1.getText().equals("Sehat")){
-            penyebabpenyakit1.setText("sehat coooooooyyyyyyyyy");
-        }
+        penyebabpenyakit1.setText(penyebabPenyakit);
+//        if (namaPenyakit1.getText().equals("Scabies")){
+//            penyebabpenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
+//        } else if (namaPenyakit1.getText().equals("Gastritis")){
+//            penyebabpenyakit1.setText("Ini deskripsi gastritis");
+//        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
+//            penyebabpenyakit1.setText("helmiiiiiiinnnnnnnnnn");
+//        } else if (namaPenyakit1.getText().equals("Rhintis")){
+//            penyebabpenyakit1.setText("rhintiiiiiiiiissssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermatitis")){
+//            penyebabpenyakit1.setText("dermatitiiiiisssssssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
+//            penyebabpenyakit1.setText("dermaphytttttttttttttttttttt");
+//        } else if (namaPenyakit1.getText().equals("Otitis")){
+//            penyebabpenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
+//        } else if (namaPenyakit1.getText().equals("Entritis")){
+//            penyebabpenyakit1.setText("entririiiiiiiiiiiiii");
+//        } else if (namaPenyakit1.getText().equals("Sehat")){
+//            penyebabpenyakit1.setText("sehat coooooooyyyyyyyyy");
+//        }
     }
 
 
     private void setSolusiPenyakit(String solusiPenyakit){
         TextView solusipenyakit1 = findViewById(R.id.solusiPenyakit);
-//        solusipenyakit1.setText(solusiPenyakit);
-        if (namaPenyakit1.getText().equals("Scabies")){
-            solusipenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
-        } else if (namaPenyakit1.getText().equals("Gastritis")){
-            solusipenyakit1.setText("Ini deskripsi gastritis");
-        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
-            solusipenyakit1.setText("helmiiiiiiinnnnnnnnnn");
-        } else if (namaPenyakit1.getText().equals("Rhintis")){
-            solusipenyakit1.setText("rhintiiiiiiiiissssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermatitis")){
-            solusipenyakit1.setText("dermatitiiiiisssssssssssss");
-        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
-            solusipenyakit1.setText("dermaphytttttttttttttttttttt");
-        } else if (namaPenyakit1.getText().equals("Otitis")){
-            solusipenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
-        } else if (namaPenyakit1.getText().equals("Entritis")){
-            solusipenyakit1.setText("entririiiiiiiiiiiiii");
-        } else if (namaPenyakit1.getText().equals("Sehat")){
-            solusipenyakit1.setText("sehat coooooooyyyyyyyyy");
-        }
+        solusipenyakit1.setText(solusiPenyakit);
+//        if (namaPenyakit1.getText().equals("Scabies")){
+//            solusipenyakit1.setText("Scabies atau kudis ini disebabkan oleh tungu kecil yang bernama notodes coti. Tungau ini mirip dengan tungau sarcoptic yang menyebabkan scabies");
+//        } else if (namaPenyakit1.getText().equals("Gastritis")){
+//            solusipenyakit1.setText("Ini deskripsi gastritis");
+//        } else if (namaPenyakit1.getText().equals("Helminthiasis")){
+//            solusipenyakit1.setText("helmiiiiiiinnnnnnnnnn");
+//        } else if (namaPenyakit1.getText().equals("Rhintis")){
+//            solusipenyakit1.setText("rhintiiiiiiiiissssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermatitis")){
+//            solusipenyakit1.setText("dermatitiiiiisssssssssssss");
+//        } else if (namaPenyakit1.getText().equals("Dermaphytosis")){
+//            solusipenyakit1.setText("dermaphytttttttttttttttttttt");
+//        } else if (namaPenyakit1.getText().equals("Otitis")){
+//            solusipenyakit1.setText("otitiiiiiiiiiiiiiisssssssss");
+//        } else if (namaPenyakit1.getText().equals("Entritis")){
+//            solusipenyakit1.setText("entririiiiiiiiiiiiii");
+//        } else if (namaPenyakit1.getText().equals("Sehat")){
+//            solusipenyakit1.setText("sehat coooooooyyyyyyyyy");
+//        }
     }
 
 
